2cb21eb7869b5f1873c0c69b3dad215c1cba4c4b
Dan Rudolf <danrudolf@gmail.com>
2014-05-29 12:48:47 -0500

    Initial Commit

---
+++ MapSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapSearch
+//
+//  Created by dan rudolf on 5/29/14.
+//  Copyright (c) 2014 Dan Rudolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapSearch
+//
+//  Created by dan rudolf on 5/29/14.
+//  Copyright (c) 2014 Dan Rudolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
